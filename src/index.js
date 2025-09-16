8c4d0744c45c10e8778b6a197519e6afa1f63f18
Karthi-jerry <karthikutty1040746@gmail.com>
2025-09-16 22:06:59 +0530

    Add navbar and fix styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
