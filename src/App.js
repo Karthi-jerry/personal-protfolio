c5f2285eadb171c4acd6a00695a5b7ab6e3698e8
Karthi-jerry <karthikutty1040746@gmail.com>
2026-03-19 20:47:18 +0530

    force redeploy with eslint fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+// eslint-disable-next-line no-unused-vars
 import { FaReact, FaJava, FaGithub, FaLinkedin, FaBootstrap } from "react-icons/fa";
+// eslint-disable-next-line no-unused-vars
 import { SiSpringboot, SiMysql, SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
 
 import Navbar from "./Components/Navbar";
